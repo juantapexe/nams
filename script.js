f8c4af8d7cfb049f0f5e2aa9044c3bd423975a84
juantapexe <126685049+juantapexe@users.noreply.github.com>
2024-02-14 04:40:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
     // Function to fetch Valentine's Day message from the server
     function fetchValentineMessage() {
         var xhr = new XMLHttpRequest();
-        xhr.open("GET", "valentines.py", true); // Adjust the path if needed
+        xhr.open("GET", "main.py", true); // Adjust the path if needed
         xhr.onreadystatechange = function() {
             if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
                 // Update the valentine-message div with the received message
