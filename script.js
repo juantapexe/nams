38aa651b2995984b2a589784c7b0a6368437766c
juantapexe <126685049+juantapexe@users.noreply.github.com>
2024-02-14 02:43:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Function to fetch Valentine's Day message from the server
     function fetchValentineMessage() {
         var xhr = new XMLHttpRequest();
-        xhr.open("GET", "valentines.py", true); // Adjust the path if needed
+        xhr.open("GET", "valentines.py", true);
         xhr.onreadystatechange = function() {
             if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
-                // Update the valentine-message div with the received message
                 document.getElementById("valentine-message").innerText = xhr.responseText;
             }
         };
         xhr.send();
     }
 
-    // Fetch the Valentine's Day message when the page loads
+    
     fetchValentineMessage();
 });
