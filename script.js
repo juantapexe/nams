b339ad77701550d04e8a5590d8c3107cc93068b6
juantapexe <126685049+juantapexe@users.noreply.github.com>
2024-02-14 05:18:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.getElementById('showSurprise').addEventListener('click', function() {
   var surpriseDiv = document.getElementById('surprise');
-  if (surpriseDiv.style.display === 'none') {
+  if (surpriseDiv.style.display === 'none' || surpriseDiv.style.display === '') {
     surpriseDiv.style.display = 'block';
   } else {
     surpriseDiv.style.display = 'none';
