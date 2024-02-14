caf3ac03130a2b7a3682a6b01c287db7033e0217
juantapexe <126685049+juantapexe@users.noreply.github.com>
2024-02-14 07:42:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 // Define an array of random phrases
 const phrases = [
   "You mean everything to me.",
-  "but there has been a lil bit of error.",
+  "But there has been a lil bit of error.",
   "You're the best thing that's ever happened to me.",
   "Every moment with you is a blessing.",
   "You light up my world.",
-  "i didnt time it right.",
+  "I didnt time it right.",
   "I'm so grateful to have you in my life.",
-  "i actually got you something.",
+  "I actually got you something.",
   "You're the reason I smile every day.",
   "I cherish every moment we spend together."
 ];
@@ -37,6 +37,14 @@ const images = [
   "IMG_1937.jpeg",
   "IMG_1945.jpeg",
   "IMG_6353.jpeg",
+  "IMG_1424.jpeg",
+  "72793484987__07C4C528-83C1-4284-A062-3EF04A3B8B6E.jpeg",
+  "IMG_0896.jpeg",
+  "IMG_5371.jpeg",
+  "IMG_5944.jpeg",
+  "IMG_6114.jpeg",
+  "IMG_6515.jpeg",
+  "IMG_8075.jpeg",
   // Add more image URLs as needed
 ];
 
